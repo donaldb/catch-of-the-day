0354077080e864759afcdc2a25b248f088787843
Don Braithwaite <don.braithwaite@outlook.com>
2016-11-29 09:22:52 -0500

    added all files to repo

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyBA-Ops1qO8SOs9g-HBfHiWF2yQdKNyfuQ",
+    authDomain: "catch-of-the-day-dbraithwaite.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-dbraithwaite.firebaseio.com",
+})
+
+export default base;
